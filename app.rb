e433e8e368f188ba90af503aaa91bc5ecd293a62
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-02-21 11:21:22 +0000

    removes commented code

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require_relative("controllers/transactions_controller.rb")
 require_relative("controllers/budget_controller.rb")
 require_relative("controllers/tags_controller.rb")
 
+# Tag.delete_all()
 # User.delete_all()
 # Merchant.delete_all()
 # Transaction.delete_all()
